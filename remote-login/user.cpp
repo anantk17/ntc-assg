4ed326282ad6b202b2d8388730f53f836425869f
Mahesh S R <mahepj94@gmail.com>
2014-11-18 19:19:07 +0530

    added crypto

--- remote-login/user.cpp
+++ remote-login/user.cpp
@@ -2,19 +2,21 @@
 #include<vector>
 #include<string>
 #include<fstream>
+#include<iomanip>
+#include<sstream>
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
 
 #define filename "users"
-
 using namespace std;
 
-struct User {
+struct entry {
 	string username, password;
 };
 
-namespace custom
-{
-vector<User> get_users_from_file() {	//making the vector for username, password
-	vector<User> e;
+vector<entry> make() {
+	vector<entry> e;
 	
 	ifstream file;
 	file.open(filename);
@@ -22,11 +24,11 @@ vector<User> get_users_from_file() {	//making the vector for username, password
 	string user, pass;
 	
 	while(!file.eof()) {
-		User u;
+		entry en;
 		file>>user>>pass;
-		u.username = user;
-		u.password = pass;
-		e.push_back(u);
+		en.username = user;
+		en.password = pass;
+		e.push_back(en);
 	}
 	
 	file.close();
@@ -34,55 +36,41 @@ vector<User> get_users_from_file() {	//making the vector for username, password
 	return e;
 }
 
-void add_user(string username, string password, vector<User> *e) {	//adding username, password to vector and file
+void add_user(string username, string password, vector<entry> *e) {	
 	ofstream file;
 	file.open(filename, ios::app);
 	file<<username<<endl;
 	file<<password<<endl;
 	file.close();
 	
-	User u;
-	u.username = username;
-	u.password = password;
-	(*e).push_back(u);
+	entry en;
+	en.username = username;
+	en.password = password;
+	(*e).push_back(en);
 }
 
-bool exists(User u, vector<User> e) {	//checking if it is valid user
+int compare(string username, string password, vector<entry> e) {
 	for(int i=0; i<(int)e.size(); ++i)
-		if(u.username==e[i].username && u.password==e[i].password)
-			return true;
-	return false;
+		if(username==e[i].username && password==e[i].password)
+			return 1;
+	return 0;
 }
 
-User parseauthstring(string auth_string)
-    {
-        User u;
-        size_t pos;
-        string delimiter = "$";
-        pos = auth_string.find(delimiter);
-        if(pos != string::npos)
-        {
-            auth_string.erase(0,pos+delimiter.length());
-            pos = auth_string.find(delimiter);
-            if(pos != string::npos)
-            {
-                u.username = auth_string.substr(0,pos);
-                auth_string.erase(0, pos+delimiter.length());
-                u.password = auth_string;
-            }
-            else
-            {
-                u.username = "";
-                u.password = "";
-            }
+void sha1(string password) {
+	unsigned char obuf[20];
+	SHA1((unsigned char*)password.c_str(), password.size(), obuf);
+	string s="";
+	
+	int i;
+	
+ 	for (i = 0; i < 20; i++) {
+ 		s+=obuf[i];
         }
-        else
-        {
-            u.username = "";
-            u.password = "";
-        }
-
-        return u;
-    }
-    
+        
+	stringstream ss;
+	for(int i=0; i<20; ++i)
+	    ss << std::hex << (int)obuf[i];
+	string mystr = ss.str();
+	cout<<mystr<<endl;
+    	printf("\n");
 }
